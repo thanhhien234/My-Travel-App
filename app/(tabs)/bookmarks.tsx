af263d58bbaf574666a2c418319820366c471872
thanhhien234 <doanthanhhien01@gmail.com>
2024-06-03 20:46:52 +0900

    feat: add tabs

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Page = () => {
     return (
         <View style={styles.container}>
-            <Text>Index</Text>
+            <Text>Bookmarks</Text>
         </View>
     );
 };
