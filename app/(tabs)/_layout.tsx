53362e8af2395b8c3eb65d2ce3ddb6d003030254
thanhhien234 <doanthanhhien01@gmail.com>
2024-06-04 16:22:33 +0900

    feat: add icon for tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,46 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name = "index"/>
-      <Tabs.Screen name = "category"/>
-      <Tabs.Screen name = "profile"/>
-      <Tabs.Screen name = "bookmarks"/>
-      <Tabs.Screen name = "search"/>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="compass" color={ color } size={25} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="category"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="funnel" color={ color } size={25} />
+          )
+        }} />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" color={ color } size={25} />
+          )
+        }} />
+      <Tabs.Screen
+        name="bookmarks"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="bookmark" color={ color } size={25} />
+          )
+        }} />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="search" color={color} size={25} />
+          )
+        }} />
     </Tabs>
   );
 }
